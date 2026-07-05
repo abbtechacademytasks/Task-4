28b03a05f819a10d9298b38cd56bbfed76263d21
Vagif Pashayev (PC) <pashayev.vaqif2005@rambler.ru>
2026-07-05 11:31:16 +0400

    Added third task solution

---
+++ src/Task3.java
@@ -0,0 +1,27 @@
+public class Task3 {
+    public static void main(String[] args) {
+        String[] words = {"telefon", "televizor", "teleskop"};
+
+        System.out.println(checkPrefix(words));
+    }
+
+    public static String checkPrefix(String[] words) {
+        String result = "";
+
+        if (words.length == 0 || words[0].isBlank()) return result;
+
+        for (int i = 1; i <= words[0].length(); i++) {
+            String prefix = words[0].toLowerCase().substring(0, i);
+
+            for (String word : words) {
+                if (!word.toLowerCase().startsWith(prefix)) {
+                    return result;
+                }
+            }
+
+            result = words[0].substring(0, i);
+        }
+
+        return result;
+    }
+}
